9bc97162f29c737527a227b25816973b60c57ca2
jnfdev <jairo.jnf.2010@gmail.com>
2022-11-16 08:36:45 -0500

    Stablish relationship between User and Course.

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use App\Models\Course;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -43,6 +45,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }    
 
     protected function isAdmin(): Attribute
     {
