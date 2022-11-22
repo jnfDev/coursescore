0c57191d75c4af631ecb39fe2cb1788fdbf99143
jnfdev <jairo.jnf.2010@gmail.com>
2022-11-22 13:55:29 -0500

    Add Courses entry on the Admin Header.

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ auth, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('courses.index')} active={route().current('courses.index')}>
+                                    Courses
+                                </NavLink>
                             </div>
                         </div>
 
