6369ae33b4eb8649790b0e44036e084f6e9a80fb
jnfdev <jairo.jnf.2010@gmail.com>
2022-12-26 16:25:06 -0500

    Adding Publish status to ModelStatus enum.

--- app/Enums/ModelStatus.php
+++ app/Enums/ModelStatus.php
@@ -4,6 +4,7 @@ namespace App\Enums;
 
 enum ModelStatus: string
 {
+    case Publish          = 'publish';
     case WaitingForCreate = 'waiting-for-create';
     case WaitingForUpdate = 'waiting-for-update';
     case WaitingForDelete = 'waiting-for-delete';
@@ -11,6 +12,7 @@ enum ModelStatus: string
     public function label()
     {
         return match($this) {
+            ModelStatus::Publish          => 'Publish',
             ModelStatus::WaitingForCreate => 'Waiting for creation',
             ModelStatus::WaitingForUpdate => 'Waiting for update',
             ModelStatus::WaitingForDelete => 'Waiting for deletion',
